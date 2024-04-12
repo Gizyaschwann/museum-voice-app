c3d45dfe9425bb5880f58b543f974e451a14e101
Ilyuza Miranda-Gizzatullina <gizyaschwann@gmail.com>
2024-04-12 04:47:30 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './parrish_logo.svg';
+import './App.css';
+import {Auth} from "./components/auth";
+import React, {useState} from 'react'
+import { VoiceRecorder } from 'react-voice-recorder-player';
+import AudioRecorder from "../src/AudioRecorder";
+import {MenuItem, Select} from "@mui/material";
+
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <img src={logo} alt="parrish_logo" className="responsive"/>
+            </header>
+            <div>
+                <h1>THE ART OF FOOD</h1>
+                <h2>Please select one of the following questions from the dropdown menu and record your answer:</h2>
+                <Select displayEmpty>
+                    <MenuItem value="">
+                        <em>Press here to select the question</em>
+                    </MenuItem>
+                    <MenuItem value={1}>What is the most delicious thing you have ever eaten? Describe it!</MenuItem>
+                    <MenuItem value={2}>What role does food play in your family's cultural or holiday
+                        traditions?</MenuItem>
+                    <MenuItem value={3}>Share a memorable food from your childhood.</MenuItem>
+                </Select>
+                <div>
+                    <AudioRecorder/>
+                </div>
+            </div>
+            {/*<div>*/}
+            {/*    <h1>React Voice Recorder</h1>*/}
+            {/*    <VoiceRecorder/>*/}
+            {/*</div>*/}
+            {/*<Auth/>*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA2H3WEmfcopErGgPve_SHcnW1j2lr-mQw",
+    authDomain: "museumvoiceapp.firebaseapp.com",
+    projectId: "museumvoiceapp",
+    storageBucket: "museumvoiceapp.appspot.com",
+    messagingSenderId: "766867072821",
+    appId: "1:766867072821:web:cdbc48989aa7ecd8ad4e7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
